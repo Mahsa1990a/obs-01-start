064b085f5b0da6771a56fa8af6e281344b241b4a
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-04 16:07:57 -0400

    174. Building a Custom Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs'; //interval is an observable
+import { interval, Subscription, Observable } from 'rxjs'; //interval is an observable
 
 @Component({
   selector: 'app-home',
@@ -14,11 +14,25 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    // wanna new observable made by myself
     // interval(1000) => every sec a new event would be emitted
-    this. firstObservableSubscription = interval(1000).subscribe(count => {
-      console.log("COUNT => ", count);
+    // this. firstObservableSubscription = interval(1000).subscribe(count => {
+    //   console.log("COUNT => ", count);
+    // });
+
+    // wanna new observable made by myself, not coming from angular:
+    const customIntervalObservable = Observable.create((observer) => { //observer interested being inform about data,errors,completion of observable
+      let count = 0;
+      setInterval(() => {//repeat every sec
+        // observer.error() //error => to throw an err
+        // observer.complete() //complete => completion of observable
+        observer.next(count); //next => to emit a new value
+        count ++;
+      }, 1000);
     });
+    // now subscribe to our custom observable:
+    this.firstObservableSubscription = customIntervalObservable.subscribe(data => {
+      console.log("DATA => ", data)
+    })
   }
 
   // lifecycle hook
