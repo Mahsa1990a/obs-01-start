0bdb2379b3464f05a5f3dee361ffbc96aab16abf
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-04 12:41:30 -0400

    173. Getting Closer to the Core of Observables(using interval observable from rxjs third party package and have to unsubscribe it)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs'; //interval is an observable
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  // for clearing observable:
+  private firstObservableSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    // wanna new observable made by myself
+    // interval(1000) => every sec a new event would be emitted
+    this. firstObservableSubscription = interval(1000).subscribe(count => {
+      console.log("COUNT => ", count);
+    });
+  }
+
+  // lifecycle hook
+  ngOnDestroy(): void {
+    //whenever we leave component(route) to go to another component(route), clear that subscribtion
+    this.firstObservableSubscription.unsubscribe();
   }
 
 }
